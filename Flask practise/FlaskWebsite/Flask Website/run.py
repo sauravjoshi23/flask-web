328a7274a6f606e9d7d9646fc1bc1fb83cc0e208
Saurav Joshi <44198980+sauravjoshi23@users.noreply.github.com>
2019-07-12 12:19:53 +0530

    Update run.py

--- Flask practise/FlaskWebsite/Flask Website/run.py
+++ Flask practise/FlaskWebsite/Flask Website/run.py
@@ -1,4 +1,6 @@
+import os
 from flaskwebsite import app
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 #run
